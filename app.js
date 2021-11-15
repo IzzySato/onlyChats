7fde21a236d73da2bd8de69c8a33d518bb286629
IzzySato <48259693+IzzySato@users.noreply.github.com>
2021-11-15 13:50:46 -0800

    removed database

--- app.js
+++ app.js
@@ -5,18 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
-//const http = require('http');
 const app = express();
-//const server = http.createServer(app);
-
-const mongoose = require('mongoose');
-const session = require('express-session');
-const passport = require('passport');
-const passportLocalMongoose = require('passport-local-mongoose');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-
-const findOrCreate = require('mongoose-findorcreate');
 
 const indexRouter = require('./routes/index');
 const signupRouter = require('./routes/signup');
@@ -24,52 +13,6 @@ const loginRouter = require('./routes/login');
 const logoutRouter = require('./routes/logout');
 const userRouter = require('./routes/user');
 const chatRouter = require('./routes/chat');
-
-app.use(session({
-  secret: 'Our littee secret.',
-  resave: false,
-  saveUninitialized: false
-}));
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-mongoose.connect('mongodb://localhost:27017/onlyChats', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-
-//mongoose.set('useCreateIndex', true);
-
-
-const userSchema = new mongoose.Schema({
-  userFirstName: String,
-  userLastName: String,
-  email: String,
-  password: String
-});
-
-userSchema.plugin(passportLocalMongoose);
-userSchema.plugin(findOrCreate);
-
-const User = mongoose.model('User', userSchema);
-
-passport.use(User.createStrategy());
-passport.serializeUser((user, done) => {
-  done(null, user.id);
-});
-
-passport.deserializeUser((id, done) => {
-  User.findById(id, (err, user) => {
-    done(err, user);
-  });
-});
-
-app.use('/', (req, res, next) => {
-  req.User = User;
-  req.passport = passport;
-  next();
-});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/signup.js
+++ routes/signup.js
@@ -8,27 +8,6 @@ router.get('/', (req, res) => {
 
 //POST register a new user
 router.post('/', (req, res) => {
-  const { body: { userFirstName, userLastName, username, password }} = req;
-  console.log(`New register for ${userFirstName} / ${userLastName} / ${username} / ${password}`);
-  try {
-    req.User.register({ userFirstName,
-                        userLastName,
-                        username, 
-                        email: username
-                      }, password, (err, user) => {
-      if(err){
-        console.log(`Error ${err}`);
-        res.redirect('/');
-      }else{
-        req.passport.authenticate('local')(req, res, () => {
-        res.json({url: '/'});
-        });
-      }
-    });    
-  } catch (error) {
-    console.log(error);
-  }
-
 });
 
 module.exports = router;
